60cbf15042f7e0a1bd13e40cc3e1e3381faf4426
lucas150 <henil.gandhi2003@gmail.com>
2025-05-28 11:39:38 +0530

    react-web-2

--- src/app/components/SignedCall.js
+++ src/app/components/SignedCall.js
@@ -13,8 +13,8 @@ export default function SignedCall() {
         .then((clevertap) => {
           return import("clevertap-signed-call").then(({ initSignedCall }) => {
             initSignedCall({
-              // accountId: "61a52046f56a14cb19a1e9cc",
-              // apikey: "9dcced09dae16c5e3606c22346d92361b77efdb360425913850bea4f22d812dd",
+              // accountId: "",
+              // apikey: "",
               cuid: "CTHenil",
               clevertap: clevertap.default, // Make sure to access the default export
               name: "CTHenil",
