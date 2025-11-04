c543d417fba6ccecb189e2d1d4070701a411b78f
Henil Gandhi <henil.gandhi2003@gmail.com>
2025-11-04 20:08:12 +0530

    activity changed

--- android/app/src/main/java/com/projectreactnative/MainActivity.kt
+++ android/app/src/main/java/com/projectreactnative/MainActivity.kt
@@ -1,9 +1,18 @@
 package com.projectreactnative
 
+import android.content.Intent
+import android.os.Build
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
+
+
+import com.clevertap.react.CleverTapPackage;
+import com.clevertap.android.sdk.ActivityLifecycleCallback;
+import com.clevertap.android.sdk.CleverTapAPI;
+import com.clevertap.android.sdk.CleverTapAPI.LogLevel;
+import com.clevertap.react.CleverTapApplication
 
 class MainActivity : ReactActivity() {
 
@@ -15,4 +24,15 @@ class MainActivity : ReactActivity() {
 
   override fun createReactActivityDelegate(): ReactActivityDelegate =
       DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        var cleverTapDefaultInstance = CleverTapAPI.getDefaultInstance(applicationContext)
+
+        // On Android 12 and onwards, raise notification clicked event and get the click callback
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            cleverTapDefaultInstance?.pushNotificationClickedEvent(intent!!.extras)
+        }
+    }
+
 }
--- android/app/src/main/java/com/projectreactnative/MainApplication.kt
+++ android/app/src/main/java/com/projectreactnative/MainApplication.kt
@@ -34,6 +34,7 @@ class MainApplication : CleverTapApplication(), ReactApplication {
 
         override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
         override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
+
       }
 
   override val reactHost: ReactHost
